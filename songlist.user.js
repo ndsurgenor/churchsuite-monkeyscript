c528d72333021ee014606553309d29df663c824e
ndsurgenor <ndsurgenor@gmail.com>
2023-11-16 20:53:39 +0000

    Initial commit

---
+++ songlist.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name        ChurchSuite Song View Updater
+// @homepage    https://github.com/ndsurgenor/churchsuite-monkeyscript
+// @copyright   Nathan Surgenor 2023
+// @namespace   http://tampermonkey.net/
+// @version     Alpha
+// @description Adds 'Last Used' and 'Keys' columns to Song list on ChurchSuite
+// @author      Nathan Surgenor
+// @match       https://*.churchsuite.com/planning/songs
+// @grant       none
+// @run-at      document body
+// ==/UserScript==
+
+
+(function() {
+    'use strict';
+
+    // Inserts new table headings of 'Last used and 'Keys'
+
+    // Inserts new columns for the new headings
+
+    // Inserts the data relevant to the new columns
+
+})();

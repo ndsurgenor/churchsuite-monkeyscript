184b453dd5c7937c4b9114586e7fbada9f08cda4
ndsurgenor <ndsurgenor@gmail.com>
2023-11-16 21:11:19 +0000

    Add GM_log message to test script is running (passed)

--- songlist.user.js
+++ songlist.user.js
@@ -2,18 +2,21 @@
 // @name        ChurchSuite Song View Updater
 // @homepage    https://github.com/ndsurgenor/churchsuite-monkeyscript
 // @copyright   Nathan Surgenor 2023
+// @icon        blob:chrome-extension://dhdgffkkebhmkfjojejmpbldmpobfkfo/96723154-5d32-4c12-a5c1-dc4b0d6ccccd
 // @namespace   http://tampermonkey.net/
-// @version     Alpha
+// @version     Alpha-v1
 // @description Adds 'Last Used' and 'Keys' columns to Song list on ChurchSuite
 // @author      Nathan Surgenor
 // @match       https://*.churchsuite.com/planning/songs
-// @grant       none
-// @run-at      document body
+// @grant       GM_log
+// @run-at      document-body
 // ==/UserScript==
 
 
 (function() {
     'use strict';
+
+    GM_log('Script Running')
 
     // Inserts new table headings of 'Last used and 'Keys'
 
